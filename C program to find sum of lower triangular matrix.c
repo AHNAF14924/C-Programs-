bf0dcdf4ca53d1d62da7e707016bca9d031956b5
AHNAF TAHMID <ahnaftahmid14924@gmail.com>
2020-05-05 15:44:16 +0600

    Add files via upload

---
+++ C program to find sum of lower triangular matrix.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define size 3
+int main()
+{
+    int a[size][size],row,col,sum=0;
+    printf("Enter elements of a array:\n");
+    for(row=0;row<size;row++)
+    {
+        for(col=0;col<size;col++)
+        {
+            scanf("%d",&a[row][col]);
+        }
+    }
+    for(row=0;row<size;row++)
+    {
+        for(col=0;col<size;col++)
+        {
+            if(col<row)
+            {
+                sum+=a[row][col];
+            }
+        }
+    }
+    printf("\n%d\n",sum);
+    return 0;
+}
+
